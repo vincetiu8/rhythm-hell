c74334cab6c7725f67e85ef0a8753974dc33e9b7
Dyhuan <55378629+Dyhuan@users.noreply.github.com>
2022-02-21 01:28:43 +0800

    Main menu (#11)
    * misspelled
    
    * added graphics to main menu

--- Assets/Scripts/Enemy/VerticalShotFire.cs
+++ Assets/Scripts/Enemy/VerticalShotFire.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VerticalshotFire : MonoBehaviour
+public class VerticalShotFire : MonoBehaviour
 
 {
     [SerializeField] private int bulletAmount;
