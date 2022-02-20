6b228f4ecedf78b713ac3461f201c7dd30babf59
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2022-02-21 03:24:46 +0800

    add health bar back

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using UI;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,23 +7,36 @@ namespace Player
 {
     public class PlayerHealth : MonoBehaviour
     {
-        [SerializeField] private int playerHealth;
+        [SerializeField] private int initialHealth;
         [SerializeField] private GameObject gameOverPrefab;
         [SerializeField] private Text timerText;
+        [SerializeField] private RectTransform healthBar;
+        [SerializeField] private float regenHealthRate;
     
         private int _health;
+        private float _initialWidth;
+        private float _carryHealth;
 
         private void Awake()
         {
-            _health = playerHealth;
+            _health = initialHealth;
+            _initialWidth = healthBar.sizeDelta.x;
+        }
+
+        private void Update()
+        {
+            _carryHealth += regenHealthRate * Time.deltaTime;
+            ChangeHealth((int) _carryHealth);
+            _carryHealth %= 1;
         }
 
         public void ChangeHealth(int change)
         {
-            _health += change;
+            _health = Math.Min(_health + change, initialHealth);
+
+            healthBar.sizeDelta = new Vector2(_initialWidth * _health / initialHealth, healthBar.sizeDelta.y);
 
             if (_health > 0) return;
-            
             Destroy(gameObject);
             Counter.CounterInstance.StopIncrement();
             timerText.text = Counter.CounterInstance.GetTime().ToString();
