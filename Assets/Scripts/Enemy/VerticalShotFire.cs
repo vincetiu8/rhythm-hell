e95f882020f7be2fcab0bed6540665ee8031b5db
Dyhuan <55378629+Dyhuan@users.noreply.github.com>
2022-02-21 00:12:42 +0800

    machine gun fire added (#8)

---
+++ Assets/Scripts/Enemy/VerticalShotFire.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalShotFire : MonoBehaviour
+
+{
+    [SerializeField] private int bulletAmount;
+
+    private Vector2 _bulletDirection;
+
+    private void Start()
+    {
+        InvokeRepeating("SpreadFire", 0f, 0.3f);
+    }
+
+    private void SpreadFire()
+    {
+
+        float interval = bulletAmount;
+        
+        for (int i = 0; i < bulletAmount + 1; i++)
+        {
+            float directionX = transform.position.x;
+            float directionY = transform.position.y + 5;
+
+            Vector3 movementVector = new Vector3(directionX, directionY, 0f);
+            Vector2 bulletDirection = (movementVector - transform.position).normalized;
+
+            GameObject bulletSpawn = GetComponentInChildren<BulletStorage>().GetBullet();
+            bulletSpawn.transform.position = transform.position;
+            bulletSpawn.SetActive(true);
+            bulletSpawn.GetComponent<Bullet>().SetDirection(bulletDirection);
+        }
+    }
+}
